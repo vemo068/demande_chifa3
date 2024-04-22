ca99e1ad91414acc4b0b84e6d1d258417dc033f4
vemo068 <66393949+vemo068@users.noreply.github.com>
2024-04-22 13:11:12 +0100

    files

--- lib/models/assure.dart
+++ lib/models/assure.dart
@@ -1,6 +1,44 @@
 class Assure {
-  String? num_Assure;
-  String? nom;
-  String? password;
+  String? id_user;
+  String num_Assure;
+  String nom;
+  String prenom;
+  String date_nai;
+  String password;
 
+  Assure({
+    this.id_user,
+    required this.num_Assure,
+    required this.nom,
+    required this.prenom,
+    required this.date_nai,
+    required this.password,
+  });
+
+  factory Assure.fromJson(Map<String, dynamic> json) {
+    return Assure(
+      id_user: json['id_user'],
+      num_Assure: json['num_Assure'],
+      nom: json['nom'],
+      prenom: json['prenom'],
+      date_nai: json['date_nai'],
+      password: json['password'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {
+      'num_Assure': num_Assure,
+      'nom': nom,
+      'prenom': prenom,
+      'date_nai': date_nai,
+      'password': password,
+    };
+    if (id_user != null) {
+      data['id_user'] = id_user;
+    }
+    return data;
+  }
+
+  // Other necessary functions can be added here as per requirement
 }
---
+++ lib/models/demande_renouvle.dart
@@ -0,0 +1,11 @@
+
+import 'package:demande_chifa/models/carte.dart';
+import 'package:file_picker/file_picker.dart';
+
+class DemandeRenouvle {
+  String? id;
+  PlatformFile? attestation_img;
+  PlatformFile? assure_img;
+  PlatformFile? id_img;
+  Carte? carte;
+}
